7fa9d820bb59b8ad27a607a961f1b3fd0f7308e4
Marie-Carleen Frei <lene@7freiheiten.de>
2021-06-01 19:24:18 +0200

    Update cards routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,38 +1,29 @@
 const express = require('express')
-const cards = require('../cards.json')
 const router = express.Router()
+const Card = require('../models/Card')
 
-router.get('/', (req, res, next) => {
-  res.send(cards)
+router.get('/', async (req, res, next) => {
+  res.json(await Card.find().populate('author', 'name -_id'))
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   const { id } = req.params
-  const card = cards.find(card => card.id === id)
-  card ? res.send(card) : next()
+  res.json(await Card.findById(id).populate('author', 'name -_id'))
 })
 
-router.post('/', (req, res, next) => {
-  const newCard = req.body
-  cards.push(newCard)
-  res.status(201).json(newCard)
+router.post('/', async (req, res, next) => {
+  res.status(201).json(await Card.create(req.body))
 })
 
-router.patch('/:id', (req, res, next) => {
+router.patch('/:id', async (req, res, next) => {
+  const { id } = req.params
+  res.json(await Card.findByIdAndUpdate(id, req.body, { new: true }))
+})
+
+router.delete('/:id', async (req, res, next) => {
   const { id } = req.params
 
-  const index = cards.findIndex(card => card.id === id)
-  const card = cards[index]
-  const updatedCard = { ...card, ...req.body }
-  cards.splice(index, 1, updatedCard)
-
-  card ? res.status(200).json(updatedCard) : next()
-})
-
-router.delete('/:id', (req, res, next) => {
-  const { id } = req.params
-  cards.filter(card => card.id !== id)
-  res.sendStatus(204)
+  res.status(204).json(await Card.findByIdAndDelete(id))
 })
 
 module.exports = router
